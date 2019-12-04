e7ea87bf25d593b93291b2fad3ebae1977a7e4e0
Diego Osorio <diegoso-0989@hotmail.com>
2019-12-04 08:34:02 -0500

    Separadores y listas vacias

--- src/videos/containers/SuggestionList.js
+++ src/videos/containers/SuggestionList.js
@@ -3,17 +3,22 @@ import {
     FlatList,
     Text
 } from 'react-native';
+
 import Layout from '../components/SuggestionListLayout';
+import Empty from '../components/Empty';
+import Separator from '../components/VerticalSeparator'
 
 class SuggestionList extends Component {
+    renderEmpty = () => <Empty text="No hay sugerencias" />
+    itemSeparator = () => <Separator color="red"/>
     render(){
         const list = [
             {
-                title: 'Titulo1',
+                title: 'Avengers',
                 key: '1'
             },
             {
-                title: 'Titulo2',
+                title: 'Pokemon',
                 key: '2'
             }
         ];
@@ -24,6 +29,8 @@ class SuggestionList extends Component {
             >
                 <FlatList 
                     data={list}
+                    ListEmptyComponent={this.renderEmpty}
+                    ItemSeparatorComponent={this.itemSeparator}
                     renderItem={({ item }) => <Text>{item.title}</Text>}
                 />  
             </Layout>
