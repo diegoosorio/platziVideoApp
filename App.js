95b7419cb9d48374a55edb69a30fd98ef7f3f705
Diego Osorio <diegoso-0989@hotmail.com>
2019-12-29 12:24:33 -0500

    Corriegiendo nombres en rutas de componentes

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import {
   Text,
 } from 'react-native';
 
-import Home from './src/screens/containers/home';
-import Header from './src/sections/components/header';
-import SuggestionList from './src/videos/containers/suggestion-list';
+import Home from './src/screens/containers/Home';
+import Header from './src/sections/components/Header';
+import SuggestionList from './src/videos/containers/SuggestionList';
 import API from './utils/api';
 
 // type Props = {};
