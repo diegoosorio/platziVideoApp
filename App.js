43a6b6332df4909cb08b0d0d9e45448acd8ecd0d
Diego Osorio <diegoso-0989@hotmail.com>
2020-01-14 20:55:00 -0500

    Instalando plugins de la comunidad, react-native-video

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 
 import Home from './src/screens/containers/Home';
 import Header from './src/sections/components/Header';
 import SuggestionList from './src/videos/containers/SuggestionList';
 import API from './utils/api';
 import CategoryList from './src/videos/containers/CategoryList';
+import Video from 'react-native-video';
 
 // type Props = {};
 
@@ -29,6 +30,28 @@ export default class App extends Component {
     return (
       <Home>
         <Header />
+        <View
+          // eslint-disable-next-line react-native/no-inline-styles
+          style={{
+            flex: 1,
+            height: 100,
+          }}>
+          <Video
+            source={{
+              uri:
+                'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+            }}
+            // eslint-disable-next-line react-native/no-inline-styles
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              bottom: 0,
+              top: 0,
+            }}
+            resizeMode="contain"
+          />
+        </View>
         <Text>buscador</Text>
         <CategoryList list={this.state.categoryList} />
         <SuggestionList list={this.state.suggestionList} />
