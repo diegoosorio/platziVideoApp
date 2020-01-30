e64012ac7fd8b2a78762537feadc73c91219ad67
Diego Osorio <diegoso-0989@hotmail.com>
2020-01-29 21:33:17 -0500

    Controles del reproductor

--- src/player/components/Layout.js
+++ src/player/components/Layout.js
@@ -1,17 +1,12 @@
-/* eslint-disable prettier/prettier */
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 function Layout(props) {
   return (
     <View style={styles.container}>
       <View style={styles.video}>{props.video}</View>
-      <View style={styles.overlay}>
-        {
-          props.loading &&
-          props.loader
-        }
-      </View>
+      <View style={styles.overlay}>{props.loading && props.loader}</View>
+      {props.controls}
     </View>
   );
 }
@@ -32,11 +27,11 @@ const styles = StyleSheet.create({
     position: 'absolute',
     left: 0,
     top: 0,
-    right: 0,
     bottom: 0,
+    right: 0,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
 });
 
 export default Layout;
--- src/player/containers/Player.js
+++ src/player/containers/Player.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
-import {StyleSheet, ActivityIndicator} from 'react-native';
+import {StyleSheet, ActivityIndicator, Text} from 'react-native';
 import Video from 'react-native-video';
 import Layout from '../components/Layout';
+import ControlLayout from '../components/ControlLayout';
+import PlayPause from '../components/PlayPause';
 
 class Player extends Component {
   state = {
     loading: true,
+    paused: false,
   };
 
   onBuffer = ({isBuffering}) => {
@@ -17,6 +20,12 @@ class Player extends Component {
   onLoad = () => {
     this.setState({
       loading: false,
+    });
+  };
+
+  playPause = () => {
+    this.setState({
+      paused: !this.state.paused,
     });
   };
 
@@ -35,9 +44,18 @@ class Player extends Component {
             resizeMode="contain"
             onBuffer={this.onBuffer}
             onLoad={this.onLoad}
+            paused={this.state.paused}
           />
         }
         loader={<ActivityIndicator color="red" />}
+        controls={
+          <ControlLayout>
+            <PlayPause onPress={this.playPause} paused={this.state.paused} />
+            <Text>progress bar |</Text>
+            <Text>time left |</Text>
+            <Text>fullscreen |</Text>
+          </ControlLayout>
+        }
       />
     );
   }
